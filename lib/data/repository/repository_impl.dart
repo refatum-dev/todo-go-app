933eef6a7318991ca6667a4034c203eb60dfea23
refatum-dev <refatum.develop@gmail.com>
2023-01-07 03:47:17 +0100

    implement Repository in repository_impl.dart

---
+++ lib/data/repository/repository_impl.dart
@@ -0,0 +1,17 @@
+import 'package:todogo_app/domain/model/task.dart';
+import 'package:todogo_app/domain/repository/repository.dart';
+
+class RepositoryImpl implements Repository {
+  @override
+  Future<int> createTask() {
+    // TODO: implement createTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Task>> getAllTasks() {
+    // TODO: implement getAllTasks
+    throw UnimplementedError();
+  }
+
+}
