99081f8c10b978de9e90a10562e1f2bc5e448746
refatum-dev <refatum.develop@gmail.com>
2023-01-07 03:43:46 +0100

    implement Task model in task_entity.dart

--- lib/data/entity/task_entity.dart
+++ lib/data/entity/task_entity.dart
@@ -1,7 +1,25 @@
 import 'package:equatable/equatable.dart';
+import 'package:todogo_app/domain/model/task.dart';
 
-class TaskEntity extends Equatable {
+class TaskEntity extends Equatable implements Task {
+  const TaskEntity(
+      this.id, this.title, this.description, this.dateTime, this.isCompleted);
+
   @override
-  List<Object?> get props => throw UnimplementedError();
+  final int id;
 
+  @override
+  final String title;
+
+  @override
+  final String description;
+
+  @override
+  final DateTime dateTime;
+
+  @override
+  final bool isCompleted;
+
+  @override
+  List<Object?> get props => [id];
 }
